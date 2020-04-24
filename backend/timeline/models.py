310e770bbf89cc745f72f3e70c2dfb68765148ae
Shashank Kumar <theshashank6870@gmail.com>
2020-04-25 03:03:47 +0530

    Timeline model added

--- backend/timeline/models.py
+++ backend/timeline/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Timeline(models.Model):
+
+    GOAL_CHOICES = (
+        ('L', 'Longterm'),
+        ('S', 'Shortterm'),
+    )
+    STATUS_CHOICES = (
+        ('O','Ongoing'),
+        ('E', 'Ended'),
+        ('D', 'Dropped')
+    )
+
+    user = models.ForeignKey(User,related_name='timeline', 
+        on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    goal_term = models.CharField(max_length=1, choices=GOAL_CHOICES)
+    status = models.CharField(max_length=1,choices=STATUS_CHOICES)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
